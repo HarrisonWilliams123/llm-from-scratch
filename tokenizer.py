5a2aa6a1ea482f805a46b5b84822eeff571454b5
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-08-30 23:10:52 -0400

    Created the initializer for the tokenization process

--- tokenizer.py
+++ tokenizer.py
@@ -8,5 +8,12 @@ I learned there are multiple methods for tokenization.
 3. Subword level: Using algorithms like Byte-Pair Encoding (which I'm using in this script)
     to split common words and keep rare words flexible.
 """
+import tiktoken
 class Tokenizer():
-    pass
+   def __init__(self):
+      #Maps the token id to the token string ({11246: "some"})
+      self.vocab = {}
+      #Maps token string to token id ({"some": 11246})
+      self.inverse_vocab = {}
+      #Dictionary of BPE merges: {(token_id1, token_id2): merged_token_id}
+      self.bpe_merges = {}
