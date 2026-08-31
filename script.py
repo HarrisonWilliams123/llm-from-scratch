0c2563c5af14ed90236a67ebfe4bd17249dd47ec
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-08-30 21:57:56 -0400

    Inputted the data to be used for the llm

---
+++ script.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#Used the words from AlgoMonster for data because I couldn't come with other words to use
+data = ['the', 'cat', 'dog', 'sat', 'ran', 'on', 'mat', 'house', 'a', 'big',
+        'small', 'quickly', 'slowly', 'and', 'is', 'red', 'blue', 'to',
+        'PAD', 'END']
+
