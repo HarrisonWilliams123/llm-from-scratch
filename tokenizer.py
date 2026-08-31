f2e04cb99faa5f5d516c83dd5ec523b08b51267c
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-08-30 23:00:24 -0400

    Documented the different methods of tokenization

--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,12 @@
+"""
+I learned there are multiple methods for tokenization.
+
+1. Character Level: Break the words down into individual letters
+    Ex: "dog" becomes d, o, g 
+2. Word Level: Split text by spaces into whole words
+    Ex: "the dog sat on a mat" becomes the, dog, sat, on, a, mat
+3. Subword level: Using algorithms like Byte-Pair Encoding (which I'm using in this script)
+    to split common words and keep rare words flexible.
+"""
 class Tokenizer():
-    
+    pass
