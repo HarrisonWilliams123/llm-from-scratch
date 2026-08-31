4eb8bef9bdab490972c6a91685c7d1f5bcbd3598
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-08-30 22:04:19 -0400

    Created a venv environment and changed the name of the data/vocab

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-#Used the words from AlgoMonster for data because I couldn't come with other words to use
-data = ['the', 'cat', 'dog', 'sat', 'ran', 'on', 'mat', 'house', 'a', 'big',
+#Used the words from AlgoMonster for the vocab because I couldn't come with other words to use
+vocab = ['the', 'cat', 'dog', 'sat', 'ran', 'on', 'mat', 'house', 'a', 'big',
         'small', 'quickly', 'slowly', 'and', 'is', 'red', 'blue', 'to',
         'PAD', 'END']
 
+print(vocab)
